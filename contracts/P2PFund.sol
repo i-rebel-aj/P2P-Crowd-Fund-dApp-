df66c6809543f710c2adccc3ffe3409eb292c38b
Akshay Jain <akshayjain1999.aj18@gmail.com>
2023-04-02 04:05:50 +0530

    feat(erc20): Added ERC 20 Token Contract and Investment and Release Funds Method

--- contracts/P2PFund.sol
+++ contracts/P2PFund.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract P2PFund{
     
     address public FundTokenAddress; 
@@ -18,8 +20,8 @@ contract P2PFund{
         uint256 fund_end_date;
         //ProjectFundType fund_type;
         uint256 debt_annual_interest_rate;
-        uint256 debt_end_date;
-        uint256 debt_amount; 
+        uint256 debt_repayment_date;
+        uint256 fund_target; 
     }
     struct Investment{
         uint256 investment_amount;
@@ -37,20 +39,25 @@ contract P2PFund{
         FundTokenAddress=_FundTokenAddress;
     }
 
-    function addNewProject(string memory project_name, uint256 fund_start_date, uint256 fund_end_date, uint256 debt_annual_interest_rate, uint256 debt_end_date, uint256 debt_amount) public {
-        
+    modifier messageSenderNotZero(){
+        require(msg.sender != address(0), "Transaction can not be made by 0 address");
+        _;
+    }
+
+    function addNewProject(string memory project_name, uint256 fund_start_date, uint256 fund_end_date, uint256 debt_annual_interest_rate, uint256 debt_end_date, uint256 fund_target) public {
+    
         Project memory newProject;
         newProject.project_name=project_name;
         newProject.fund_start_date=fund_start_date;
         newProject.fund_end_date=fund_end_date;
         newProject.debt_annual_interest_rate=debt_annual_interest_rate;
-        newProject.debt_end_date=debt_end_date;
-        newProject.debt_amount=debt_amount;
+        newProject.debt_repayment_date=debt_end_date;
+        newProject.fund_target=fund_target;
 
         if( !(newProject.fund_start_date > block.timestamp && newProject.fund_end_date > newProject.fund_start_date) ){
             revert("Start Date must be greater than current date and end date must be greater than start date"); 
         }
-        if(newProject.debt_end_date < newProject.fund_end_date){
+        if(newProject.debt_repayment_date < newProject.fund_end_date){
             revert("Debt repayment date can not be less than fund end date");
         }
         newProject.project_id=projects.length +1;
@@ -59,13 +66,13 @@ contract P2PFund{
         projects_mapping[newProject.project_id]=newProject;
     }
 
-    function investInProject(uint256 projectId, uint256 amount) public {
-        require(projects_mapping[projectId].project_owner!=address(0), "Property with given id not found");
+    function investInProject(uint256 projectId, uint256 amount) public messageSenderNotZero{
+        require(projects_mapping[projectId].project_owner!=address(0), "Project with given id not found");
         require(projects_mapping[projectId].project_owner!=msg.sender, "Investor can not be project owner himself");
 
         uint256 alreadyInvestedAmount=project_to_net_investment_map[projectId];
-        if(amount>projects_mapping[projectId].debt_amount-alreadyInvestedAmount){
-            revert("Amount that is being invested can not exceed max debt amount required");
+        if(amount>projects_mapping[projectId].fund_target-alreadyInvestedAmount){
+            revert("Amount that is being invested can not exceed max fund target required");
         }
         
         Investment memory newInvestment;
@@ -76,6 +83,12 @@ contract P2PFund{
 
         //TODO: Do Token Transfer Logic Here
         investments_mapping[msg.sender].push(newInvestment);
+        IERC20(FundTokenAddress).transfer(address(this), amount);
     }
 
+    function releaseFundsToProjectOwner(uint256 projectId) public messageSenderNotZero{
+        require(projects_mapping[projectId].project_owner==msg.sender, "Project's owner is not message sender");
+        require(projects_mapping[projectId].fund_end_date<block.timestamp, "Fund End Date Has Not Elapsed yet");
+        IERC20(FundTokenAddress).transfer(msg.sender, project_to_net_investment_map[projectId]);
+    }
 }
