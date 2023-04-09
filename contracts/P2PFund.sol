cdb5a57106831f3641948fe7ecd82155632961ef
Akshay Jain <akshayjain1999.aj18@gmail.com>
2023-04-09 19:24:24 +0530

    refactor(contract): Made error messages constant for better eadability feat(contract): Emiting Required Events test(contract): Added Test Cases on Adding Property and Investing In Project

--- contracts/P2PFund.sol
+++ contracts/P2PFund.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract P2PFund{
     
     address public FundTokenAddress; 
@@ -35,6 +36,21 @@ contract P2PFund{
     mapping(uint256 => Project) public projects_mapping;
     mapping(address => mapping(uint256=>uint256)) project_net_invested_by_investor;
     mapping(uint256 => uint256) public project_to_net_investment_map; 
+    
+    /**
+    * Events
+    */
+    event NewProjectAdded(uint256 project_id);
+    event NewInvestmentMade(address investor_address, uint256 project_id, uint256 amount);
+    /**
+        Constant Error Messages
+    */
+    string constant private ERROR_MSG_SENDER_ZERO="Transaction can not be made by 0 address";
+    string constant private ERROR_PROJECT_NOT_FOUND="Project with given id not found";
+    string constant private ERROR_NOT_PROJECT_OWNER="Project's owner is not message sender";
+    string constant private ERROR_NOT_PROJECT_INVESTOR="investor with given address has not invested in the project";
+    string constant private ERROR_FUND_NOT_STARTED="Fundraise for the given project not started yet";
+    string constant private ERROR_PROJECT_OWNER_NOT_PERMITTED="Investor can not be project owner himself";
 
     constructor(address _FundTokenAddress){
         require(_FundTokenAddress != address(0), "Fund Token Address Can Not Be A Zero Addresss");
@@ -42,7 +58,7 @@ contract P2PFund{
     }
 
     modifier messageSenderNotZero(){
-        require(msg.sender != address(0), "Transaction can not be made by 0 address");
+        require(msg.sender != address(0), ERROR_MSG_SENDER_ZERO);
         _;
     }
 
@@ -52,7 +68,8 @@ contract P2PFund{
         newProject.project_name=project_name;
         newProject.fund_start_date=fund_start_date;
         newProject.fund_end_date=fund_end_date;
-        newProject.debt_annual_interest_rate=debt_annual_interest_rate;
+        //Handling Upto 2 Decimal Places
+        newProject.debt_annual_interest_rate=debt_annual_interest_rate*100;
         newProject.debt_repayment_date=debt_end_date;
         newProject.fund_target=fund_target;
 
@@ -66,12 +83,13 @@ contract P2PFund{
         newProject.project_owner=msg.sender;
         projects.push(newProject);
         projects_mapping[newProject.project_id]=newProject;
+        emit NewProjectAdded(newProject.project_id);
     }
 
     function investInProject(uint256 projectId, uint256 amount) public messageSenderNotZero{
-        require(projects_mapping[projectId].project_owner!=address(0), "Project with given id not found");
-        require(projects_mapping[projectId].project_owner!=msg.sender, "Investor can not be project owner himself");
-
+        require(projects_mapping[projectId].project_owner!=address(0), ERROR_PROJECT_NOT_FOUND);
+        require(projects_mapping[projectId].project_owner!=msg.sender, ERROR_PROJECT_OWNER_NOT_PERMITTED);
+        require(block.timestamp > projects_mapping[projectId].fund_start_date, ERROR_FUND_NOT_STARTED);
         uint256 alreadyInvestedAmount=project_to_net_investment_map[projectId];
         if(amount>projects_mapping[projectId].fund_target-alreadyInvestedAmount){
             revert("Amount that is being invested can not exceed max fund target required");
@@ -86,18 +104,20 @@ contract P2PFund{
         investor_project_mapping[msg.sender][projectId].push(newInvestment);
         project_net_invested_by_investor[msg.sender][projectId]+=amount;
         IERC20(FundTokenAddress).transfer(address(this), amount);
+
+        emit NewInvestmentMade(msg.sender, projectId, amount);
     }
 
     function releaseFundsToProjectOwner(uint256 projectId) public messageSenderNotZero{
-        require(projects_mapping[projectId].project_owner==msg.sender, "Project's owner is not message sender");
+        require(projects_mapping[projectId].project_owner==msg.sender, ERROR_NOT_PROJECT_OWNER);
         require(projects_mapping[projectId].fund_end_date<block.timestamp, "Fund End Date Has Not Elapsed yet");
         IERC20(FundTokenAddress).transfer(msg.sender, project_to_net_investment_map[projectId]);
     }
 
     function payDebtToInvestor(uint256 projectId, address investor_address) public messageSenderNotZero{
-        require(projects_mapping[projectId].project_owner==msg.sender, "Project's owner is not message sender");
+        require(projects_mapping[projectId].project_owner==msg.sender, ERROR_NOT_PROJECT_OWNER);
         require(projects_mapping[projectId].debt_repayment_date<block.timestamp, "Project Repayment Date Has Not Reached Yet");
-        require(project_net_invested_by_investor[investor_address][projectId] > 0, "investor with given address has not invested in the project");
+        require(project_net_invested_by_investor[investor_address][projectId] > 0, ERROR_NOT_PROJECT_INVESTOR);
         //Takes a year = 365.25 days accounting for leap year, thus 365.25 * 24 * 60 * 60
         uint256 totalSecondsInYear=31557600;
         uint256 amountToBePayed= project_net_invested_by_investor[investor_address][projectId] * 
--- test/P2PFundTest.ts
+++ test/P2PFundTest.ts
@@ -4,17 +4,39 @@ import { P2PFund, P2PFundToken } from '../typechain-types'
 import {Signer} from 'ethers'
 const expect = chai.expect
 const ethers=hardhat.ethers
+const currentDate=new Date()
+const Projects= [
+    {
+        name: "My First Project",
+        fund_start_date: (new Date(currentDate.getTime()+(24*60*60*1000))).getTime(),
+        fund_end_date: new Date(currentDate.getTime()+(7*24*60*60*1000)).getTime(),
+        //3 means 0.03% of annual returns 
+        debt_annual_interest_rate: 3,
+        debt_repayment_date: new Date(currentDate.getTime()+(365.25*24*60*60*1000)).getTime(),
+        fund_target: 500
+    }
+]
+
+enum revertMessages{
+    ERROR_MSG_SENDER_ZERO="Transaction can not be made by 0 address",
+    ERROR_PROJECT_NOT_FOUND="Project with given id not found",
+    ERROR_NOT_PROJECT_OWNER="Project's owner is not message sender",
+    ERROR_NOT_PROJECT_INVESTOR="investor with given address has not invested in the project",
+    FUND_NOT_STARTED="Fundraise for the given project not started yet",
+    ERROR_PROJECT_OWNER_NOT_PERMITTED="Investor can not be project owner himself"
+}
 
 describe('P2P Fund Contract',async () => {
     let contract_owner: Signer;
     let p2p_token_owner: Signer;
     let investor1: Signer;
     let investor2: Signer;
+    let project1_owner: Signer;
     let [investor1_address, investor2_address]: [string, string]=['', '']
     let P2PFundTokenContract: P2PFundToken;
     let P2PFund_Contact: P2PFund;
     before(async()=>{
-        [contract_owner, p2p_token_owner, investor1, investor2]=await ethers.getSigners()
+        [contract_owner, p2p_token_owner, investor1, investor2, project1_owner]=await ethers.getSigners()
         investor1_address=await investor1.getAddress()
         investor2_address=await investor2.getAddress()
         const P2PFund_ContractFactory=await ethers.getContractFactory('P2PFund')
@@ -40,6 +62,41 @@ describe('P2P Fund Contract',async () => {
     });
     
     describe("Adding Project", async()=>{
-        
+        it("Should Add Project 1, with correct values", async()=>{
+            const firstProjects=Projects[0]
+            const respone=await P2PFund_Contact.connect(project1_owner).addNewProject(
+                firstProjects.name,
+                firstProjects.fund_start_date,
+                firstProjects.fund_end_date,
+                firstProjects.debt_annual_interest_rate,
+                firstProjects.debt_repayment_date,
+                firstProjects.fund_target
+            )
+            expect(respone).to.emit(P2PFund_Contact, 'NewProjectAdded').withArgs(1)
+        })
+        it("Project with Id 1, must have values set as defined", async ()=>{
+            const response=await P2PFund_Contact.projects_mapping(1)
+            //console.log('Response is', response)
+            expect(response[0]).to.be.equal(1)
+            expect(response[1]).to.be.equal(Projects[0].name)
+            expect(response[2]).to.be.equal(await project1_owner.getAddress())
+            expect(response[3]).to.be.equal(Projects[0].fund_start_date)
+            expect(response[4]).to.be.equal(Projects[0].fund_end_date)
+            expect(response[5]).to.be.equal(Projects[0].debt_annual_interest_rate*100)
+            expect(response[6]).to.be.equal(Projects[0].debt_repayment_date)
+            expect(response[7]).to.be.equal(Projects[0].fund_target)
+        })
+        //TODO: Add Reverted test Cases Here
+    })
+    describe("Investing In Project 1", async()=>{
+        it("Should Revert When Project Id is Invalid", async()=>{
+            expect(P2PFund_Contact.connect(investor1).investInProject(100, 50)).to.be.revertedWith(revertMessages.ERROR_PROJECT_NOT_FOUND)
+        })
+        it("Should Revert When Project Investor is owner himself", async()=>{
+            expect(P2PFund_Contact.connect(project1_owner).investInProject(1, 50)).to.be.revertedWith(revertMessages.ERROR_PROJECT_OWNER_NOT_PERMITTED)
+        })
+        it("Should Revert When Fund has Not started", async()=>{
+            expect(P2PFund_Contact.connect(investor1).investInProject(1, 50)).to.be.revertedWith(revertMessages.FUND_NOT_STARTED)
+        })
     })
 })
